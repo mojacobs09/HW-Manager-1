3f298df80b95060be6bcf775fd60479a1353c57f
Monica-Lillie <152418293+mojacobs09@users.noreply.github.com>
2026-02-08 17:55:23 +0000

    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title = 'IST 488 HW',
 st.title ("IST 488 HW")
 HW1 = st.Page('HW/HW1.py', title = 'HW 1')
 HW2 = st.Page('HW/HW2.py', title = 'HW 2')
-HW3 = st.Page('HW/HW3.PY', title = 'HW 3', default = True)
+HW3 = st.Page('HW/HW3.py', title = 'HW 3', default = True)
 
 pg = st.navigation([HW3, HW2, HW1])
 pg.run()
